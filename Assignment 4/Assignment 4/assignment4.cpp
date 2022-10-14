528fb7b0af9ff549b2808fec949bbd389af8b17e
Wanyea <61568218+Wanyea@users.noreply.github.com>
2022-10-14 15:20:44 -0400

    Assignment 4 Finished

--- Assignment 4/Assignment 4/assignment4.cpp
+++ Assignment 4/Assignment 4/assignment4.cpp
@@ -1,48 +1,146 @@
 #include <iostream>
 #include <vector>
-#include <math.h>
-#include <glm.hpp>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm.hpp>
 #include <gtc/matrix_transform.hpp>
 #include <gtc/type_ptr.hpp>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
 
-const char* srcVS = R"HERE(
-	#version 330 core
-	layout (location = 0) in vec3 pos; 
-    layout (location = 1) in vec3 normal;
- 
-    out vec3 fragNormal;
+const char* srcVS = "#version 330 core\n"
 
-    uniform mat4 view;
-    uniform mat4 projection;
+"layout (location = 0) in vec3 pos; \n"
+"layout (location = 1) in vec3 normal; \n"
+"layout (location = 2) in vec2 uvs; \n"
 
-	void main () 
-	{
-		  gl_Position = projection * view * vec4(pos, 1);
-          fragNormal = vec3(vec4(normalize(normal), 0));
-	}
+"out vec2 fragUVs; \n"
+"out vec3 fragNormal; \n"
+"uniform mat4 view;\n"
+"uniform mat4 projection;\n"
 
-)HERE";
+"void main () {\n"
+"      gl_Position = projection * view * vec4(pos, 1); \n"
+"      fragNormal = normalize(normal); \n"
+"      fragUVs = uvs; \n"
 
-const char* srcFS = R"HERE(
-	#version 330 core
-	out vec4 FragColor;
-    in vec3 fragNormal;
+"}\n";
 
-	void main () 
-	{
-	   FragColor = vec4(abs(fragNormal), 1.0);
-	}
-)HERE";
+const char* srcFS = "#version 330 core\n"
 
-int main()
+"out vec4 outColor;\n"
+
+"in vec3 fragNormal; \n"
+"in vec2 fragUVs; \n"
+
+"uniform sampler2D tex; \n"
+
+"void main () {\n"
+"   vec4 materialColor = texture(tex, fragUVs); \n"
+"   outColor = materialColor; \n"
+"}\n";
+
+int xWindow = 700;
+int yWindow = 700;
+
+const float PI = 3.1415926535f;
+
+int whichRender = 0;
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+float camElevation = PI / 2.0f;
+float alpha = PI / 2.0f;
+
+float elevation = PI / 2.0f;
+float azimuth = PI / 2.0f;
+
+
+float deg2rad(float degree)
+{
+    return 2 * PI * (degree / 360.0f);
+}
+
+static void key_callback(GLFWwindow* window, int key, int keycode, int action, int mods)
+{
+    float cameraSpeed = static_cast<float>(deltaTime);
+    float speed = 3.0f;
+    bool ep = false, em = false, ap = false, am = false;
+
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+
+    if (key == GLFW_KEY_F && action == GLFW_PRESS) {
+        whichRender = 0;
+    }
+
+    if (key == GLFW_KEY_L && action == GLFW_PRESS) {
+        whichRender = 1;
+    }
+
+    if (key == GLFW_KEY_P && action == GLFW_PRESS) {
+        whichRender = 2;
+    }
+
+    if (key == GLFW_KEY_S && action == GLFW_PRESS && elevation < deg2rad(179)) {
+        ep = true;
+    }
+    if (key == GLFW_KEY_S && action == GLFW_RELEASE) {
+        ep = false;
+    }
+
+    if (key == GLFW_KEY_W && action == GLFW_PRESS && elevation > deg2rad(1)) {
+        em = true;
+    }
+    if (key == GLFW_KEY_W && action == GLFW_RELEASE) {
+        em = false;
+    }
+
+    if (key == GLFW_KEY_D && action == GLFW_PRESS) {
+        ap = true;
+    }
+    if (key == GLFW_KEY_D && action == GLFW_RELEASE) {
+        ap = false;
+    }
+
+    if (key == GLFW_KEY_A && action == GLFW_PRESS) {
+        am = true;
+    }
+    if (key == GLFW_KEY_A && action == GLFW_RELEASE) {
+        am = false;
+    }
+
+    if (ep)
+    {
+        elevation += cameraSpeed * speed;
+    }
+
+    if (em)
+    {
+        elevation -= cameraSpeed * speed;
+    }
+
+    if (ap)
+    {
+        azimuth += cameraSpeed * speed;
+    }
+
+    if (am)
+    {
+        azimuth -= cameraSpeed * speed;
+    }
+
+}
+
+int main(void)
 {
     // glfw: initialize and configure
-    // ------------------------------
+// ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -51,11 +149,6 @@ int main()
 
     int xWindow = 700;
     int yWindow = 700;
-
-    const float PI = 3.14159265359f;
-    int whichRender = 0;
-    float elevation = PI / 2.0f;
-    float alpha = PI / 2.0f;
 
     // glfw window creation
     // --------------------
@@ -69,7 +162,6 @@ int main()
 
     glfwMakeContextCurrent(window);
 
-
     if (glewInit() != GLEW_OK)
     {
         std::cout << "Failed to initialize GLEW" << std::endl;
@@ -77,6 +169,8 @@ int main()
     }
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetKeyCallback(window, key_callback);
+
 
     // Compile vertex shader
 
@@ -134,228 +228,179 @@ int main()
     glDeleteShader(shaderObjVS);
     glDeleteShader(shaderObjFS);
 
-    unsigned int index;
-
     std::vector<glm::vec3> positions;
     std::vector<glm::vec3> normals;
     std::vector<unsigned int> indices;
-
-
-    unsigned int stride = (3 + 3) * sizeof(float);
+    std::vector<glm::vec2> uvs;
 
     const unsigned int numOfStacks = 64;
-    const unsigned int numOfSectors = 64;
+    const unsigned int numOfSections = 64;
     unsigned int radius = 1.0f;
 
     for (unsigned int i = 0; i <= numOfStacks; ++i)
     {
-        for (unsigned int j = 0; j <= numOfSectors; ++j)
+        for (unsigned int j = 0; j <= numOfSections; ++j)
         {
-            float x = ((float)i) / ((float)numOfStacks);
-            float y = ((float)j) / ((float)numOfSectors);
+            float x = (float)i / (float)numOfStacks;
+            float y = (float)j / (float)numOfSections;
 
             float xPos = radius * (std::cos(x * 2.0f * PI) * std::sin(y * PI));
-            float yPos = radius * (std::cos(y * PI));
-            float zPos = radius * (std::sin(x * 2.0f * PI) * std::sin(y * PI));
+            float yPos = (std::cos(y * PI)) * radius;
+            float zPos = (std::sin(x * 2.0f * PI) * std::sin(y * PI)) * radius;
 
-            positions.push_back(glm::vec3(xPos, yPos, zPos));
-            normals.push_back(glm::vec3(xPos, yPos, zPos));
+            positions.emplace_back(xPos, yPos, zPos);
+            normals.emplace_back(xPos, yPos, zPos);
+            uvs.emplace_back(x, y);
         }
     }
 
     bool isOddRow = true;
 
-    for (unsigned int y = 0; y < numOfSectors; ++y)
+    for (unsigned int y = 0; y < numOfSections; ++y)
     {
-        if (isOddRow)
+        if (isOddRow) 
         {
             for (unsigned int x = 0; x <= numOfStacks; ++x)
             {
-                indices.push_back(y * (numOfStacks + 1) + x);
                 indices.push_back((y + 1) * (numOfStacks + 1) + x);
+                indices.push_back(y* (numOfStacks + 1) + x);
             }
         }
         else
         {
             for (int x = numOfStacks; x >= 0; --x)
             {
-                indices.push_back((y + 1) * (numOfStacks + 1) + x);
                 indices.push_back(y * (numOfStacks + 1) + x);
+                indices.push_back((y + 1) * (numOfStacks + 1) + x);
             }
         }
 
         isOddRow = !isOddRow;
     }
 
-    index = static_cast<unsigned int>(indices.size());
+    unsigned int indexCount;
+    indexCount = static_cast<unsigned int>(indices.size());
 
-    std::vector<float> dataPoints;
-    std::vector<glm::vec3> tangents;
-    std::vector<glm::vec3> bitangents;
-    std::vector<glm::vec3> normals2;
+    std::vector<float> data;
 
-    for (unsigned int i = 0; i < positions.size(); ++i)
+    for (unsigned int i = 0, j = 0; i < positions.size(); ++i)
     {
-        dataPoints.push_back(positions[i].x);
-        dataPoints.push_back(positions[i].y);
-        dataPoints.push_back(positions[i].z);
-
-        if (normals2.size() > 0)
-        {
-            dataPoints.push_back(normals2[i].x);
-            dataPoints.push_back(normals2[i].y);
-            dataPoints.push_back(normals2[i].z);
-        }
-
-        normals2.push_back(positions[i]);
-        normals2.emplace_back(1.0f, 0.0f, 0.0f);
-        normals2.push_back(positions[i] + (positions[i] * 0.1f));
-        normals2.emplace_back(1.0f, 0.0f, 0.0f);
-
-        glm::vec3 normCross = glm::normalize(glm::cross(positions[i], glm::vec3(0.0f, 0.0f, 1.0f)));
-        tangents.push_back(positions[i]);
-        tangents.emplace_back(0.0f, 0.0f, 1.0f);
-        tangents.push_back(positions[i] + 0.1f * normCross);
-        tangents.emplace_back(0.0f, 0.0f, 1.0f);
-
-        glm::vec3 normalCross = glm::normalize(glm::cross(positions[i], normCross));
-        bitangents.push_back(positions[i]);
-        bitangents.emplace_back(0.0f, 1.0f, 0.0f);
-        bitangents.push_back(positions[i] + 0.1f * normalCross);
-        bitangents.emplace_back(0.0f, 1.0f, 0.0f);
+        data.push_back(positions[i].x);
+        data.push_back(positions[i].y);
+        data.push_back(positions[i].z);
+        data.push_back(normals[i].x);
+        data.push_back(normals[i].y);
+        data.push_back(normals[i].z);
+        data.push_back(uvs[i].x);
+        data.push_back(uvs[i].y);
 
     }
 
-    unsigned int VAO[4], VBO[4], EBO[1];
-
-
-    // Sphere
-    glGenVertexArrays(1, &VAO[0]);
-    glGenBuffers(1, &VBO[0]);
-    glGenBuffers(1, &EBO[0]);
-    glBindVertexArray(VAO[0]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
-    glBufferData(GL_ARRAY_BUFFER, dataPoints.size() * sizeof(glm::vec3), &dataPoints[0], GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[0]);
+    unsigned int VAO = 0;
+    unsigned int stride = (3 + 3 + 2) * sizeof(float);
+    glGenVertexArrays(1, &VAO);
+    unsigned int vbo, ebo;
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, data.size() * sizeof(float), &data[0], GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride, (void*)(sizeof(glm::vec3)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride, (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride, (void*)(6 * sizeof(float)));
 
-    // Normals
-    glGenVertexArrays(1, &VAO[1]);
-    glGenBuffers(1, &VBO[1]);
-    glBindVertexArray(VAO[1]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
-    glBufferData(GL_ARRAY_BUFFER, normals2.size() * sizeof(glm::vec3), &normals2[0], GL_STATIC_DRAW);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride, (void*)(sizeof(glm::vec3)));
+    int width, height, channels;
+    unsigned char* img = stbi_load("earth_texture.jpg", &width, &height, &channels, 0);
+    if (img == NULL) {
+        printf("Error: cannot load image.\n");
+        exit(1);
+    }
 
-    // Tangents 
-    glGenVertexArrays(1, &VAO[2]);
-    glGenBuffers(1, &VBO[2]);
-    glBindVertexArray(VAO[2]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO[2]);
-    glBufferData(GL_ARRAY_BUFFER, tangents.size() * sizeof(glm::vec3), &tangents[0], GL_STATIC_DRAW);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride, (void*)(sizeof(glm::vec3)));
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
 
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-    glGenVertexArrays(1, &VAO[3]);
-    glGenBuffers(1, &VBO[3]);
-    glBindVertexArray(VAO[3]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO[3]);
-    glBufferData(GL_ARRAY_BUFFER, bitangents.size() * sizeof(glm::vec3), &bitangents[0], GL_STATIC_DRAW);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride, (void*)(sizeof(glm::vec3)));
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, img);
 
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    glEnable(GL_CULL_FACE);
+
+    glCullFace(GL_FRONT);
 
     while (!glfwWindowShouldClose(window))
     {
-
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);  
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
 
         // Poll for and process events
         glfwPollEvents();
 
+        if (whichRender == 0)
+        {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        }
+        else if (whichRender == 1)
+        {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        }
+        else if (whichRender == 2)
+        {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+            glPointSize(5.0);
+        }
+
         glUseProgram(ShaderProgram);
 
-        // Create projection 
-        glm::mat4 projection = glm::perspective(glm::radians(30.0f), (float)(xWindow / yWindow), 0.1f, 50.0f);
+        glEnable(GL_TEXTURE_2D);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glUniform1i(glGetUniformLocation(ShaderProgram, "tex"), 0);
+
+
+
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)(xWindow / yWindow), 0.1f, 100.0f);
         int projectionLocation = glGetUniformLocation(ShaderProgram, "projection");
         glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, &projection[0][0]);
 
         glm::mat4 view = glm::mat4(1.0f);
-        float radOfCamera = 6.0f;
-        float x = radOfCamera * std::sin(elevation) * std::cos(alpha);
-        float y = radOfCamera * std::cos(elevation);
-        float z = radOfCamera * std::sin(elevation) * std::sin(alpha);
-
-        view = glm::lookAt(glm::vec3(x, y, z), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        float cameraRadius = 5.0f;
+        float viewX = cameraRadius * std::sin(camElevation) * std::cos(alpha);
+        float viewY = cameraRadius * std::cos(camElevation);
+        float viewZ = cameraRadius * std::sin(camElevation) * std::sin(alpha);
+        view = glm::lookAt(glm::vec3(viewX, viewY, viewZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
         int viewLocation = glGetUniformLocation(ShaderProgram, "view");
         glUniformMatrix4fv(viewLocation, 1, GL_FALSE, &view[0][0]);
 
-        // Draw calls:
 
-        //1. Sphere
-        glBindVertexArray(VAO[0]);
-        glDrawElements(GL_TRIANGLE_STRIP, index, GL_UNSIGNED_INT, 0);
-
-        //2. Tangent Line
-        glBindVertexArray(VAO[2]);
-        glDrawArrays(GL_LINES, 0, tangents.size() / 2);
-
-        //3. Bitangent Line
-        glBindVertexArray(VAO[3]);
-        glDrawArrays(GL_LINES, 0, bitangents.size() / 2);
-
-        //4. Normal Line
-        glBindVertexArray(VAO[1]);
-        glDrawArrays(GL_LINES, 0, normals2.size() / 2);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLE_STRIP, indexCount, GL_UNSIGNED_INT, 0);
 
         glBindVertexArray(0);
         glUseProgram(0);
 
-        // Swap front and back buffers
         glfwSwapBuffers(window);
     }
 
-
-
     glfwTerminate();
+
     return 0;
-}
-
-
-
-
-void processInput(GLFWwindow* window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-
-    if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS)
-    {
-        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    }
-    else if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS) {
-        glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
-        glPointSize(15.0);
-    }
-    else if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS) {
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    }
-
-
 }
 
 
@@ -363,10 +408,3 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
-
-
-
-
-
-
-
